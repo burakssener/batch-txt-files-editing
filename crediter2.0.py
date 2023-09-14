d6ff3f24181ee7c617e6cd03689e2620270d4fd7
burakssener <ssenerburak@gmail.com>
2023-09-14 21:24:19 +0300

    this code goes into subdirectories to write the text and also check if there is text, it won't be written.

--- crediter2.0.py
+++ crediter2.0.py
@@ -1,18 +1,28 @@
-import glob
-import os   
+import os
+from pathlib import Path
+
+def append_credit_to_txt_files(folder_path):
+    # Get all .txt files recursively in the specified folder
+    txt_files = list(Path(folder_path).rglob("*.txt"))
+
+    # Extract the folder name for credit
+    folder_name = os.path.basename(folder_path)
+    credit_text = "\n\nCredit: " + folder_name
+
+    for file_path in txt_files:
+        try:
+            # Check if the credit text already exists in the file
+            with open(file_path, "r", encoding="utf-8") as file:
+                content = file.read()
+                if credit_text not in content:
+                    # Append the credit text to the file
+                    with open(file_path, "a", encoding="utf-8") as file:
+                        file.write(credit_text + "\n")
+        except UnicodeDecodeError:
+            print(f"Error reading file: {file_path}. Skipping.")
 
 # Specify the folder path where your .txt files are located
-folder_path = 'C:/Users/burak/Desktop/instaloader/babybearyuki'
+folder_path = 'C:/Users/burak/Desktop/crediter'
 
-# Use glob to get a list of all .txt files in the folder
-txt_files = glob.glob(f"{folder_path}/*.txt")
-
-# Loop through each .txt file and append text
-folder_name= os.path.basename(folder_path)
-text_to_append = "\n\nCredit: " + folder_name
-
-for file_name in txt_files:
-    with open(file_name, "a") as file:
-        file.write(text_to_append + "\n")
-
-
+# Call the function to append credit to all .txt files in subdirectories
+append_credit_to_txt_files(folder_path)
